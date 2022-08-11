25c2875686f9dbe3a2be6a6b884b37a067bb35ce
Rafael <rafael-srv@hotmail.com>
2022-08-11 20:46:41 -0300

    Subindo pastas

---
+++ Objeto/Objeto.js
@@ -0,0 +1,6 @@
+const Pessoa ={
+    idade: 25,
+    altura: 180,
+    peso: 90
+}
+console.log(Pessoa)
---
+++ Objeto/aff.js
@@ -0,0 +1,6 @@
+const Pessoa ={
+    idade: 24,
+    altura: 180,
+    peso: 90
+}
+console.log(Pessoa)
