49371c5d15bb73e8d9f64ce761bc8ef888b4c9de
Rafael <rafael-srv@hotmail.com>
2022-08-12 22:25:20 -0300

    Map2

---
+++ Array/map2.js
@@ -0,0 +1,13 @@
+const carrinho =[
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 43.45}',
+    '{"nome": "Cante", "preco": 5.45}',
+]
+
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
