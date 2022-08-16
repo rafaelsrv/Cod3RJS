dfd93644c6f4b49581a5429be54f54dff5f773a5
Rafael <rafael-srv@hotmail.com>
2022-08-15 22:39:42 -0300

    Questão 5

---
+++ QuestõesLista2/Questão5.js
@@ -0,0 +1,10 @@
+function ver(num1,num2){
+    let variavel = true
+    if((num1===num2)||num1>num2){
+        variavel = true
+        console.log(variavel)
+    }else {
+        variavel = false
+        console.log(variavel)}
+}
+ver(1,[1])
