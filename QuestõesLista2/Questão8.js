43242058b0d5d0a949c219f634dc3d43e4396b5e
Rafael <rafael-srv@hotmail.com>
2022-08-17 20:39:35 -0300

    Questão 8

--- QuestõesLista2/Questão8.js
+++ QuestõesLista2/Questão8.js
@@ -1,1 +1,21 @@
-//Receber 4 números, para saber qual o mínimo e máximo.
+Inclusivo = false
+function verificar(nMenor,nMeio,nMaior,Inclusivo){
+    
+    if(Inclusivo == true)
+        {
+            if(nMenor<=nMeio && nMaior>=nMeio){
+                return true;
+            }else{
+                return false;
+            }
+        
+    }if(Inclusivo == false){
+        if(nMenor<nMeio && nMaior>nMeio){
+            return true;
+        }else{
+            return false;
+        }
+    
+}
+}
+console.log(verificar(18,18,40,false))
