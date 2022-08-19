fc2be84085f12cb43943cb740393ce5f05745936
Rafael <rafael-srv@hotmail.com>
2022-08-18 23:48:58 -0300

    Questão9

--- QuestõesLista2/Questão9.js
+++ QuestõesLista2/Questão9.js
@@ -1,9 +1,11 @@
 
 let resultado =0
 function multiplicar(n1,n2){
-    for(let i=1; i<n1; i++){
+    for(let i=1; i<=n2; i++){
         resultado +=n1
-        return resultado
-    }}
+        console.log(resultado)
+        
+        
+    }return resultado}
 
 console.log(multiplicar(5,5))
