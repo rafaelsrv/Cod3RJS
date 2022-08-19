dc603011c609f340676c3361ae8a6fc7d9ca4e88
Rafael <rafael-srv@hotmail.com>
2022-08-18 23:45:14 -0300

    Questão9

--- QuestõesLista2/Questão9.js
+++ QuestõesLista2/Questão9.js
@@ -1,1 +1,9 @@
-//Realizar a multiplicação de 2 número sem usar o operador *
+
+let resultado =0
+function multiplicar(n1,n2){
+    for(let i=1; i<n1; i++){
+        resultado +=n1
+        return resultado
+    }}
+
+console.log(multiplicar(5,5))
