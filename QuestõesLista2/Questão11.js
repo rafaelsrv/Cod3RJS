6885535f6ba32ccbd9b90edbd1e8e20c5e2b8fe5
Rafael <rafael-srv@hotmail.com>
2022-08-21 22:19:07 -0300

    Questão11

--- QuestõesLista2/Questão11.js
+++ QuestõesLista2/Questão11.js
@@ -1,1 +1,9 @@
-//questão11
+
+
+function elementosExtremos(vetor){
+    PrimeiroElemento = vetor[0]
+    IndiceUltimoElemento = vetor.length -1
+    UltimoElemento = vetor[IndiceUltimoElemento]
+    return Elementos=[PrimeiroElemento,UltimoElemento]
+}
+console.log(elementosExtremos([2,3,4]))
