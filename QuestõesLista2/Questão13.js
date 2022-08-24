e242713ffeedf4088a5c080fd1ed34686ad5eb1e
Rafael <rafael-srv@hotmail.com>
2022-08-24 11:40:25 -0300

    Questão13

--- QuestõesLista2/Questão13.js
+++ QuestõesLista2/Questão13.js
@@ -1,1 +1,17 @@
-//novaqvcx
+
+function vetor (vetorr){
+    let resultante = []
+    for(let i=0;i<vetorr.length; i++){
+        
+         if(typeof vetorr[i] === "string"){
+            
+            resultante.push(vetorr[i])}
+        //     resultante.push(vetorr[i])}
+
+        //     return console.log(resultante)
+        
+    }return console.log(resultante)
+    
+    
+}
+vetor([10,2,"5","6",5,"Agora Foi?"])
