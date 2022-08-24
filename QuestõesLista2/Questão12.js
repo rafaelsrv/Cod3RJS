c902248225b7ccff3c9d35785b3e91053a9a431e
Rafael <rafael-srv@hotmail.com>
2022-08-24 11:08:51 -0300

    Questão12

--- QuestõesLista2/Questão12.js
+++ QuestõesLista2/Questão12.js
@@ -1,1 +1,8 @@
-//novodsadasVASCvcxbnv
+function deletarUltimo( obj ){
+    const novo = Object.assign({}, obj)
+    console.log(`Deletando ${novo.Altura}`)
+    delete novo.Altura
+    
+    return console.log(novo)
+}
+deletarUltimo({Idade:15, Altura:150},)
