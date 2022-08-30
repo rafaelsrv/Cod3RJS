6633714cdb3135ff56f9080d4e86d89fd209825d
Rafael <rafael-srv@hotmail.com>
2022-08-29 21:58:13 -0300

    Questão29F

--- QuestõesLista2/Questão29.js
+++ QuestõesLista2/Questão29.js
@@ -3,14 +3,14 @@ function filtro(vetor, tamanho){
    let Resultado = []
    let nova = []
 
-   let transfString = vetor.map(variavel=>variavel.toString())
-   let StringSplitada = transfString.map(varievel=>varievel.split(''))
+   let transfString = vetor.map(variavel=>variavel.toString())              //Passei para string, dessa forma consigo splitar
+   let StringSplitada = transfString.map(varievel=>varievel.split(''))      //Splitei, dessa forma consigo ver o tamanho
    
-   let Mapeando = StringSplitada.map(function(StringsSeparadas){
-    if (StringsSeparadas.length == tamanho){
-        Resultado.push(StringsSeparadas)
-        nova = Resultado.map(ResultadoFinal=>ResultadoFinal.join(''))
-    }return nova})
+   let Mapeando = StringSplitada.map(function(StringsSeparadas){            //Verificando o tamanho
+    if (StringsSeparadas.length == tamanho){                
+        Resultado.push(StringsSeparadas)                                    //Se o tamanho for igual ao da variável "tamanho", push no array Resultado
+        nova = Resultado.map(ResultadoFinal=>ResultadoFinal.join(''))       //Juntando os caracteres dentro do array.
+    }})
    
    
    console.log(nova)
@@ -24,4 +24,4 @@ function filtro(vetor, tamanho){
 //     console.log(Resultado)
 }
 
-filtro([500,600,7005,50000,1,20], 5)
+filtro([500,600,7005,50000,1,20], 1)
