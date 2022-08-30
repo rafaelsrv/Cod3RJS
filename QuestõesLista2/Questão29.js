671b34f8fe6f0628c7f709961d2f6aa82a7951c7
Rafael <rafael-srv@hotmail.com>
2022-08-29 21:56:09 -0300

    Questão29F

--- QuestõesLista2/Questão29.js
+++ QuestõesLista2/Questão29.js
@@ -1,2 +1,27 @@
-const coisa = [1,2,3,4444,5555]
-console.log(coisa[4])fxzvfdasf
+function filtro(vetor, tamanho){
+
+   let Resultado = []
+   let nova = []
+
+   let transfString = vetor.map(variavel=>variavel.toString())
+   let StringSplitada = transfString.map(varievel=>varievel.split(''))
+   
+   let Mapeando = StringSplitada.map(function(StringsSeparadas){
+    if (StringsSeparadas.length == tamanho){
+        Resultado.push(StringsSeparadas)
+        nova = Resultado.map(ResultadoFinal=>ResultadoFinal.join(''))
+    }return nova})
+   
+   
+   console.log(nova)
+
+//    let StringSplit = transfString.split('')
+//    for(i=0; i<vetor.length;i++){
+//    if (StringSplit.length == tamanho){
+//     Resultado.push(vetor[i])
+
+//    }}
+//     console.log(Resultado)
+}
+
+filtro([500,600,7005,50000,1,20], 5)
